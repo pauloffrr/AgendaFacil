9c0914ee67be504b4f7ad78b1924490024f62078
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-08-28 15:34:30 -0300

    feat: creating mocked notification data

---
+++ frontend/src/data/notificationsCustomerMock.js
@@ -0,0 +1,32 @@
+export const notificationsCustomerMock = [
+    {
+        id: 1,
+        type: "Cancelamento",
+        message: "João Silva cancelou seu agendamento para o dia 28/08/2025 às 14:00.",
+        date: "28/08/2025"
+    },
+    {
+        id: 2,
+        type: "Confirmação",
+        message: "Maria Oliveira confirmou seu agendamento para o dia 30/08/2025 às 10:00.",
+        date: "28/08/2025"
+    },
+    {
+        id: 3,
+        type: "Avaliação",
+        message: "João Souza finalizou o seu serviço agendado, deseja avaliar?",
+        date: "29/08/2025"
+    },
+    {
+        id: 4,
+        type: "Concluído",
+        message: "João Souza finalizou o seu serviço agendado.",
+        date: "29/08/2025"
+    },
+    {
+        id: 5,
+        type: "Lembrete",
+        message: "Lembrete: Você tem um agendamento com Ana Paula amanhã 30/08/2025 às 16:00.",
+        date: "29/08/2025"
+    }
+]
--- frontend/src/screens/Customer/Notification.js
+++ frontend/src/screens/Customer/Notification.js
@@ -1,16 +1,50 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import CustomerNavigationBar from "../../components/CustomerNavigationBar";
+import Logo from "../../components/Logo";
+import UserIcon from "../../components/UserIcon";
 
-export default function Notification() {
+export default function Notification({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Notification Screen</Text>
+    <View style={styles.screen}>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Logo />
+                <UserIcon />
+            </View>
+
+            <Text style={styles.title}>Notificações</Text>
+            <View style={styles.separator} />
+        </View>
+
+      <CustomerNavigationBar navigation={navigation}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1
   },
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  title: {
+    fontWeight: "700",
+    fontSize: 22,
+    marginBottom: 30,
+    marginTop: 50,
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: "#D9D9D9"
+  }
 });
