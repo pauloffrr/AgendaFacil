83d7dbad12142eebee39f6c3aee89efb756f20d2
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-08-28 15:41:13 -0300

    feat: adapting mocked data

---
+++ frontend/src/components/NotificationCustomer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+export default function NotificationCustomer() {
+    return (
+        <View style={styles.container}>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20
+    }
+})
--- frontend/src/data/notificationsCustomerMock.js
+++ frontend/src/data/notificationsCustomerMock.js
@@ -3,30 +3,40 @@ export const notificationsCustomerMock = [
         id: 1,
         type: "Cancelamento",
         message: "João Silva cancelou seu agendamento para o dia 28/08/2025 às 14:00.",
-        date: "28/08/2025"
+        date: "28/08/2025",
+        professionId: 1,
+        professionName: "Eletricista"
     },
     {
         id: 2,
         type: "Confirmação",
         message: "Maria Oliveira confirmou seu agendamento para o dia 30/08/2025 às 10:00.",
-        date: "28/08/2025"
+        date: "28/08/2025",
+        professionId: 32,
+        professionName: "Cabeleireiro"
     },
     {
         id: 3,
         type: "Avaliação",
         message: "João Souza finalizou o seu serviço agendado, deseja avaliar?",
-        date: "29/08/2025"
+        date: "29/08/2025",
+        professionId: 3,
+        professionName: "Pintor"
     },
     {
         id: 4,
         type: "Concluído",
         message: "João Souza finalizou o seu serviço agendado.",
-        date: "29/08/2025"
+        date: "29/08/2025",
+        professionId: 3,
+        professionName: "Pintor"
     },
     {
         id: 5,
         type: "Lembrete",
-        message: "Lembrete: Você tem um agendamento com Ana Paula amanhã 30/08/2025 às 16:00.",
-        date: "29/08/2025"
+        message: "Lembrete: Você tem um agendamento com Ana Paula amanhã 30/08/2025 às 08:00.",
+        date: "29/08/2025",
+        professionId: 19,
+        professionName: "Diarista"
     }
 ]
