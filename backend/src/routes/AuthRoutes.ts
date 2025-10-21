12c2f3a5e134cb0dfc0fe993ff04fec384a33a5d
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-10-21 16:54:12 -0300

    refactor: deleting files that are not being used

--- backend/src/routes/AuthRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { login } from "../controllers/AuthController";
-
-const r = Router();
-r.post("/login", login);
-
-export default r;
--- backend/src/utils/id.ts
+++
@@ -1,2 +0,0 @@
-let _id = 3;
-export const nextId = () => ++_id;
