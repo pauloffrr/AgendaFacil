531ab2506ee0b583f4f46a89a71306c87e4acae6
milenaa052 <milenasantosdeoliveira40@gmail.com>
2025-10-30 21:48:23 -0300

    feat: creating routes for company

--- backend/user-service/src/company/company.service.ts
+++ backend/user-service/src/company/company.service.ts
@@ -25,7 +25,7 @@ export class CompanyService {
 
     async findAll(): Promise<Company[]> {
         return this.companyModel.findAll({
-            include: ['user']
+            include: [{ all: true }]
         });
     }
 
